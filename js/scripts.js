e35925edef732854c1756d7e93e7bd27c7be923e
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-03-30 11:36:49 -0700

    BUGFIX: fix numberOfOccurences function to prevent empty strings from passing through

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,32 @@ function wordCounter(text) {
   });
   return wordCount;
 }
+function numberOfOccurrencesInText(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element)
+  {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++
+    }
+  });
+  return wordCount;
+}
+
+// UI Logic
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+  });
+});
 
